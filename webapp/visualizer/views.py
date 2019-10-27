df1ce5c1d08e19d82d3c7b60a8b9272801132ec0
Hiroki.T <hirodora@me.com>
2019-10-27 14:10:05 +0900

    add: show face image

--- webapp/visualizer/views.py
+++ webapp/visualizer/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponseNotAllowed
 import requests
 import json
+from PIL import Image
+import urllib
+import base64
+from io import BytesIO
 
 
 def grouping(request):
@@ -29,6 +33,24 @@ def grouping(request):
         # Grouping
         headers = {'Content-Type': 'application/json'}
         grouped_faces = requests.post('http://service_face_grouping:8000/cluster/', json.dumps(faces), headers=headers).json()
+
+        # Get images as base64
+        base_url = 'http://db-controller:8888'
+        for group_id, faces in grouped_faces.items():
+            for i, face in enumerate(faces):
+                image_url = urllib.parse.urljoin(base_url, face['image_url'])
+                
+                with BytesIO() as img_fp:
+                    binary = requests.get(image_url).content
+                    img_fp.write(binary)
+                    image = Image.open(img_fp)
+
+                    with BytesIO() as onmemory_file:
+                        x, y, w, h = face['face_location']
+                        croped = image.crop((x, y, x+w, y+h))
+                        croped.save(onmemory_file, 'jpeg')
+                        b64 = base64.b64encode(onmemory_file.getvalue())
+                        grouped_faces[group_id][i]['face_image'] = str(b64)[2:-1]
 
         context = {'grouped_faces': grouped_faces}
         return render(request, 'visualizer/visualizer.html', context=context)
