8752d1a0c82d535693d524082ecb57297339080c
Hiroki.T <hirodora@me.com>
2019-10-29 22:12:16 +0900

    change: dashboard start using ajax

--- webapp/visualizer/urls.py
+++ webapp/visualizer/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'visualizer'
+
 urlpatterns = [
-    path('', views.grouping, name='visualizer-grouping'),
+    path('', views.dashboard, name='dashboard'),
+    path('select/', views.select, name='select'),
+    path('/grouping', views.grouping, name='grouping'),
 ]
--- webapp/visualizer/views.py
+++ webapp/visualizer/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseNotAllowed
+from django.http import JsonResponse
 import requests
 import json
 from PIL import Image
@@ -9,53 +10,72 @@ from io import BytesIO
 import random
 
 
-def grouping(request):
-    allowed_methods = ['GET', 'POST']
+def select(request):
+    allowed_methods = ['GET']
     if request.method not in allowed_methods:
         return HttpResponseNotAllowed(allowed_methods)
 
-    if request.method == 'GET':
-        places = requests.get('http://db-controller:8888/get_places_all').json()
-        return render(request, 'visualizer/select.html', places)
+    places = requests.get('http://db-controller:8888/get_places_all').json()
+    return render(request, 'visualizer/select.html', places)
 
-    if request.method == 'POST':
-        selected_places = request.POST.getlist('places')
-        from_date = request.POST.get('datetime-from')
-        to_date = request.POST.get('datetime-to')
 
-        # Get faces
-        get_params = {
-                'selected_places': selected_places,
-                'from_date': from_date,
-                'to_date': to_date,
-            }
-        faces = requests.get('http://db-controller:8888/get_face_encodings/', params=get_params).json()
+def dashboard(request):
+    allowed_methods = ['GET']
+    if request.method not in allowed_methods:
+        return HttpResponseNotAllowed(allowed_methods)
+    
+    get_params = request.GET.urlencode()
+    # if not selected date range & places yet
+    if get_params == '':
+        return redirect('visualizer:select')
 
-        # Grouping
-        headers = {'Content-Type': 'application/json'}
-        grouped_faces = requests.post('http://service_face_grouping:8000/cluster/', json.dumps(faces), headers=headers).json()
 
-        # Get images as base64
-        base_url = 'http://db-controller:8888'
-        for group_id, person in grouped_faces.items():
-            for i, face in enumerate(person['faces']):
-                image_url = urllib.parse.urljoin(base_url, face['image_url'])
-                
-                with BytesIO() as img_fp:
-                    binary = requests.get(image_url).content
-                    img_fp.write(binary)
-                    image = Image.open(img_fp)
+    response = render(request, 'visualizer/visualizer.html')
+    return response
 
-                    with BytesIO() as onmemory_file:
-                        top, right, bottom, left = face['face_location']
-                        croped = image.crop((left, top, right, bottom))
-                        croped.save(onmemory_file, 'jpeg')
-                        b64 = base64.b64encode(onmemory_file.getvalue())
-                        grouped_faces[group_id]['faces'][i]['face_image'] = str(b64)[2:-1]
-        
-        # Set person color (random)
-        for group_id in grouped_faces.keys():
-            grouped_faces[group_id]['person_color'] = tuple(map(lambda ab:random.randint(*ab), [(128, 255)] * 3))
 
-        context = {'grouped_faces': grouped_faces}
-        return render(request, 'visualizer/visualizer.html', context=context)
+def grouping(request):
+    allowed_methods = ['GET']
+    if request.method not in allowed_methods:
+        return HttpResponseNotAllowed(allowed_methods)
+
+    selected_places = request.POST.getlist('places')
+    from_date = request.POST.get('datetime-from')
+    to_date = request.POST.get('datetime-to')
+
+    # Get faces
+    get_params = {
+            'selected_places': selected_places,
+            'from_date': from_date,
+            'to_date': to_date,
+        }
+    faces = requests.get('http://db-controller:8888/get_face_encodings/', params=get_params).json()
+
+    # Grouping
+    headers = {'Content-Type': 'application/json'}
+    grouped_faces = requests.post('http://service_face_grouping:8000/cluster/', json.dumps(faces), headers=headers).json()
+
+    # Get images as base64
+    base_url = 'http://db-controller:8888'
+    for group_id, person in grouped_faces.items():
+        for i, face in enumerate(person['faces']):
+            image_url = urllib.parse.urljoin(base_url, face['image_url'])
+            
+            with BytesIO() as img_fp:
+                binary = requests.get(image_url).content
+                img_fp.write(binary)
+                image = Image.open(img_fp)
+
+                with BytesIO() as onmemory_file:
+                    top, right, bottom, left = face['face_location']
+                    croped = image.crop((left, top, right, bottom))
+                    croped.save(onmemory_file, 'jpeg')
+                    b64 = base64.b64encode(onmemory_file.getvalue())
+                    grouped_faces[group_id]['faces'][i]['face_image'] = str(b64)[2:-1]
+    
+    # Set person color (random)
+    for group_id in grouped_faces.keys():
+        grouped_faces[group_id]['person_color'] = tuple(map(lambda ab:random.randint(*ab), [(128, 255)] * 3))
+
+    context = {'grouped_faces': grouped_faces}
+    return JsonResponse(context, safe=False)
