909d1b66c93d8ff0c022cc23dd93bbff623f4c60
Hiroki.T <hirodora@me.com>
2019-11-20 22:43:08 +0900

    create : elm frame

--- webapp/elm-visualizer/src/Main.elm
+++ webapp/elm-visualizer/src/Main.elm
@@ -1,3 +1,5 @@
+module Main exposing (main)
+
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -5,127 +7,72 @@ import Html.Events exposing (..)
 import Http
 import Json.Decode as D exposing (Decoder)
 
+import Person exposing (..)
 
-main: Program () Model Msg
+
+-- MAIN
+
+main : Program () Model Msg
 main =
-    Browser.element{
-        init = init,
-        view = view,
-        update = update,
-        subscriptions = \_ -> Sub.none
-    }
+    Browser.element 
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
 
 
--- Model
-type alias Model =
-    {
-        input: String,
-        userState: UserState
-    }
+-- MODEL
 
-type UserState
-    = Init
-    | Waiting
-    | Loaded User
-    | Failed Http.Error
-
-init: () -> (Model, Cmd Msg)
+init : () -> (Model, Cmd Msg)
 init _ =
-    (
-        Model "" Init,
-        Cmd.none
-    )
+    ({controller = Nothing, people = Nothing}, Cmd.none)
+
+
+type alias Model =
+    { controller : Maybe ControllerModel
+    , people : Maybe (List Person)
+    }
+
+
+type alias ControllerModel =
+    { fromTimeString : String 
+    , toTimeString : String
+    , places : List Place
+    }
 
 
 
--- Update
+-- UDPATE
 
 type Msg
-    = Input String
-    | Send
-    | Receive (Result Http.Error User)
+    = SetController ControllerModel
+    | Analyze
+    | Analyzed
 
-update: Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        Input newInput ->
-            (
-                {model | input = newInput},
-                Cmd.none
-            )
-        
-        Send ->
-            (
-                {model | input = "", userState = Waiting},
-                Http.get {
-                    url = "https://api.github.com/users/" ++ model.input,
-                    expect = Http.expectJson Receive userDecoder
-                }
-            )
-        
-        Receive (Ok user) ->
-            ({model | userState = Loaded user}, Cmd.none)
-        
-        Receive (Err e) ->
-            ({model | userState = Failed e}, Cmd.none)
+        _ ->
+            (model, Cmd.none)
 
 
-view: Model -> Html Msg
+
+-- VIEW
+
+
+view : Model -> Html Msg
 view model =
-    div [] [
-        Html.form [onSubmit Send] [
-            input [
-                onInput Input,
-                autofocus True,
-                placeholder "Github name",
-                value model.input
-            ] [],
-            button [
-                disabled ((model.userState == Waiting) || String.isEmpty (String.trim model.input))
-            ] [text "Submit"]
-        ],
-        case model.userState of
-            Init ->
-                text ""
-            
-            Waiting ->
-                text "Waiting..."
-            
-            Loaded user ->
-                a [href user.htmlUrl, target "_blank"] [
-                    img [src user.avatarUrl, width 200] [],
-                    div [] [text user.name],
-                    div [] [
-                        case user.bio of
-                            Just bio ->
-                                text bio
-                            Nothing ->
-                                text ""
-                    ]
+    div [ class "container" ]
+        [ div [ class "row" ]
+            [ div [ class "col-7" ]
+                [ h1 []
+                    [ text "Map" ]
                 ]
-            
-            Failed error ->
-                div [] [text "ERROR"]
-    ]
-
-
--- DATA
-
-type alias User =
-    {
-        login: String,
-        avatarUrl: String,
-        name: String,
-        htmlUrl: String,
-        bio: Maybe String
-    }
-
-
-userDecoder: Decoder User
-userDecoder =
-    D.map5 User
-        (D.field "login" D.string)
-        (D.field "avatar_url" D.string)
-        (D.field "name" D.string)
-        (D.field "html_url" D.string)
-        (D.maybe (D.field "bio" D.string))
+            , div [ class "col-5" ]
+                [ h1 []
+                    [ text "Panel" ]
+                ]
+            ]
+        ]
