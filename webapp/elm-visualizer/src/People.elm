8fb151f11130fcadd5ea83ce1558263e5aeac67e
Hiroki.T <hirodora@me.com>
2019-11-24 11:13:47 +0900

    add: places history of each person

--- webapp/elm-visualizer/src/People.elm
+++ webapp/elm-visualizer/src/People.elm
@@ -38,7 +38,34 @@ viewPerson person =
             List.map (\f -> div [ class "col-4 face-col" ] [ imageWithBase64 f.faceImageB64 "face-img" ]) person.faces
     in
     div [ class "col-4", style "background-color" (rgbColorToCssString person.color) ]
-        [ div [ class "row face-row" ] imgs ]
+        [ div [ class "row face-row" ] imgs
+        , div [ class "row" ]
+            [ placesHistoryString person ]
+        ]
+
+
+placesHistoryString : Person -> Html msg
+placesHistoryString person =
+    let
+        faces =
+            person.faces
+
+        places =
+            List.map (\f -> f.place) faces
+
+        head =
+            List.head places
+
+        body =
+            List.drop 1 places
+                |> List.map (\p -> p.name)
+    in
+    case head of
+        Just h ->
+            text (h.name ++ List.foldl (\n p -> p ++ " -> " ++ n) "" body)
+
+        Nothing ->
+            text "No history"
 
 
 rgbColorToCssString : Data.RgbColor -> String
