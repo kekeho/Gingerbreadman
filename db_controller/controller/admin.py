8b73286cb49af9f749018f2f6f3de0285036e9b7
Hiroki.T <hirodora@me.com>
2019-11-25 13:41:03 +0900

    add: expose db_controller port to debug

--- db_controller/controller/admin.py
+++ db_controller/controller/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Date, Place, Gender, AgeGroup, Image, Face, Person
 
-# Register your models here.
+admin.site.register([Date, Place, Gender, AgeGroup, Image, Face, Person])
