8f590b6b63252a9425230e62a0190c295a15b2db
Hiroki.T <hirodora@me.com>
2019-11-27 16:49:39 +0900

    add: display place name each face

--- webapp/elm-visualizer/src/People.elm
+++ webapp/elm-visualizer/src/People.elm
@@ -37,7 +37,14 @@ viewPerson : Person -> Html msg
 viewPerson person =
     let
         imgs =
-            List.map (\f -> div [ class "col-4 face-col" ] [ imageWithBase64 f.faceImageB64 "face-img" ]) person.faces
+            List.map
+                (\f ->
+                    div [ class "col-4 face-col" ]
+                        [ imageWithBase64 f.faceImageB64 "face-img"
+                        , text f.place.name
+                        ]
+                )
+                person.faces
     in
     div [ class "col-4", style "background-color" (rgbColorToCssString person.color) ]
         [ div [ class "row face-row" ] imgs
