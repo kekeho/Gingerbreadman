3f374ff81507bfd16b9b92da107a34911f14a9ae
Hiroki.T <hirodora@me.com>
2019-11-28 17:04:48 +0900

    fix: url path

--- webapp/visualizer/urls.py
+++ webapp/visualizer/urls.py
@@ -5,6 +5,6 @@ app_name = 'visualizer'
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('get_places_all/', views.get_places_all, name='get_places_all'),
-    path('grouping/', views.grouping, name='grouping'),
+    path('visualizer/get_places_all/', views.get_places_all, name='get_places_all'),
+    path('visualizer/grouping/', views.grouping, name='grouping'),
 ]
