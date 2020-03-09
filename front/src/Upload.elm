2bd9e6a9195d67fed626f74eeaa626e4dcc1b3d1
Hiroki.T <hirodora@me.com>
2020-03-09 22:43:56 +0900

    add: files count

--- front/src/Upload.elm
+++ front/src/Upload.elm
@@ -1,84 +1,92 @@
 module Upload exposing (..)
 
+import Browser
 import CommonData exposing (..)
 import File
 import File.Select
-import Http
-import List
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
-import Browser
+import Http
+import List
+
+
 
 -- MODEL
+
 
 type alias Model =
     { places : Maybe (List Place)
     , selectedPlace : Maybe Place
     , selectedImages : Maybe (List File.File)
     , getPlacesError : Maybe Http.Error
-    , uploadResult : Maybe ((Result Http.Error ()))
+    , uploadResult : Maybe (Result Http.Error ())
     }
+
 
 
 -- UPDATE
 
+
 type Msg
     = ImageRequested
-    | ImageSelected  File.File (List File.File)
+    | ImageSelected File.File (List File.File)
     | Upload
     | Uploaded (Result Http.Error ())
     | GotPlaces (Result Http.Error (List Place))
     | PlaceSelected String
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ImageRequested -> 
-            (model, File.Select.files ["image/*"] ImageSelected)
+        ImageRequested ->
+            ( model, File.Select.files [ "image/*" ] ImageSelected )
+
         ImageSelected fstFile files ->
             ( { model | selectedImages = Just (fstFile :: files) }
             , Cmd.none
             )
-        
+
         Upload ->
             case model.selectedImages of
                 Just files ->
                     ( model
                     , Http.post
                         { url = "/api/upload/"
-                        , body = Http.multipartBody (List.map (\f -> Http.filePart "images[]" f) files )
+                        , body = Http.multipartBody (List.map (\f -> Http.filePart "images[]" f) files)
                         , expect = Http.expectWhatever Uploaded
                         }
                     )
+
                 Nothing ->
                     ( model
                     , Cmd.none
                     )
-        
+
         Uploaded result ->
             ( { model | uploadResult = Just result }, Cmd.none )
-        
+
         GotPlaces result ->
             case result of
                 Ok places ->
-                    ({ model | places = Just places, getPlacesError = Nothing }
+                    ( { model | places = Just places, getPlacesError = Nothing }
                     , Cmd.none
                     )
-                
+
                 Err error ->
-                    ({ model | getPlacesError = Just error, places = Nothing}
+                    ( { model | getPlacesError = Just error, places = Nothing }
                     , Cmd.none
                     )
-        
+
         PlaceSelected placeString ->
             let
-                place = 
+                place =
                     case model.places of
                         Just places ->
                             List.filter (\p -> p.name == placeString) places
                                 |> List.head
+
                         Nothing ->
                             Nothing
             in
@@ -88,6 +96,7 @@ update msg model =
 
 -- VIEW
 
+
 view : Model -> Browser.Document Msg
 view model =
     { title = "Upload"
@@ -96,20 +105,19 @@ view model =
             [ div [ class "row" ]
                 [ div [ class "col upload-form" ]
                     [ div [ class "form-row" ]
-                        [ div [ class "col"]
+                        [ div [ class "col" ]
                             [ h1 [ class "title" ] [ text "Upload Images" ]
                             , button [ onClick ImageRequested ] [ text "Select Images" ]
+                            , filesCountView model.selectedImages
                             ]
                         ]
-                    , div [ class "form-row"]
+                    , div [ class "form-row" ]
                         [ div [ class "col-lg-12" ]
-                            [ h2 [ ]  [  text "Select place tag" ] ]
+                            [ h2 [] [ text "Select place tag" ] ]
                         , selectPlacesView model
                         ]
                     ]
-
                 ]
-
             ]
         ]
     }
@@ -118,18 +126,39 @@ view model =
 selectPlacesView : Model -> Html Msg
 selectPlacesView model =
     div [ class "col" ]
-        [ label [] [ text "Existing Places"]
+        [ label [] [ text "Existing Places" ]
         , select [ class "form-control", onChange PlaceSelected ]
-            ( case model.places of
+            (case model.places of
                 Just places ->
-                    (List.map (\p -> option [ value p.name ] [ text p.name ]) places )
-                        
+                    List.map (\p -> option [ value p.name ] [ text p.name ]) places
+
                 Nothing ->
-                    [ ]
+                    []
             )
         ]
 
+
+filesCountView : Maybe (List File.File) -> Html Msg
+filesCountView maybeFiles =
+    Html.p []
+        (case maybeFiles of
+            Just files ->
+                let
+                    count =
+                        List.length files
+                    pluralStr =
+                        if count >= 2 then "s" else ""
+                in
+                [ text (String.fromInt count ++ " file" ++ pluralStr) ]
+
+            Nothing ->
+                []
+        )
+
+
+
 -- FUNC
+
 
 getPlaces : Cmd Msg
 getPlaces =
@@ -144,5 +173,3 @@ getPlaces =
         , timeout = Nothing
         , tracker = Nothing
         }
-
-
